ac0a8b4075e45c3efd1f2506aa71f79eb7708ebf
kunalbhole99 <kunalbhole2699@gmail.com>
2023-04-18 10:27:24 +0530

    modified

--- networking/variables.tf
+++ networking/variables.tf
@@ -15,7 +15,7 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type        = string
-  #default     = "10.10.0.0/16"
+  #default     = "10.0.0.0/16"
 }
 
 # VPC Availability Zones
@@ -29,5 +29,5 @@ variable "vpc_availability_zones" {
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type        = list(string)
-  #default     = ["10.14.101.0/24", "10.14.102.0/24"]
+  #default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
