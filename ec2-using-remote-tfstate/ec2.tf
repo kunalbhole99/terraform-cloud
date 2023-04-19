8fe6ab70813f0850a20607e8ed1ed130fdb70b9f
kunalbhole <113830783+kunalbhole99@users.noreply.github.com>
2023-04-19 17:31:33 +0530

    Update ec2.tf

--- ec2-using-remote-tfstate/ec2.tf
+++ ec2-using-remote-tfstate/ec2.tf
@@ -5,8 +5,5 @@ resource "aws_instance" "terraform-cloud-vm" {
   key_name               = "awskeypair"
   user_data              = file("install.sh")
   subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]          
-  vpc_security_group_ids = [aws_security_group.terraform-cloud-vm-sg.id,]
-  tags = {
-    "Name" = "tf-cloud-vm-using-remote-${count.index}"
-  }
+  vpc_security_group_ids = [aws_security_group.terraform-cloud-vm-sg.id]
 }
